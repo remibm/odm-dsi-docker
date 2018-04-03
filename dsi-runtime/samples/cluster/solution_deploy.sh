3ec285550e051aa6ebc2ce494b98d2660f0f5f06
remibm <36272582+remibm@users.noreply.github.com>
2018-04-03 10:24:52 +0200

    Fix/improve deployment of solution in cluster sample  (#111)

--- dsi-runtime/samples/cluster/solution_deploy.sh
+++ dsi-runtime/samples/cluster/solution_deploy.sh
@@ -3,69 +3,57 @@
 set -e
 
 function print_usage {
-        echo "USAGE: $0 <DSI_HOME>"
+        echo "USAGE: $0" 
 }
 
 function get_ip {
         echo `docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $1`
 }
 
-function get_host_port {
-        echo `docker inspect -f '{{ (index (index .NetworkSettings.Ports "9443/tcp") 0).HostPort }}' $1`
+function setvar {
+        VALUE="${@:2}"
+        eval export $1=\"$VALUE\"
+        echo "$1=$VALUE"
 }
 
-if [ -z "$1" ]; then
+function solution_deploy {
+        setvar DSI_IP "$1"
+        setvar RUNTIME_PORT "$2"
+        echo deploying solution on "$DSI_IP:$RUNTIME_PORT"
+        docker-compose run --rm -v $SRC_DIR:/dropins dsi-runtime-container1 /dsi-cmd solutionManager deploy remote /dropins/simple_solution-0.0.esa \
+        --host=$DSI_IP --port=$RUNTIME_PORT $SOL_MANAGER_OPTS || echo "Solution deployment failed on $DSI_IP"
+}
+
+function connectivity_deploy {
+        setvar DSI_IP "$1"
+        setvar INBOUND_PORT "$2"
+        echo deploying connectivity on "$DSI_IP:$INBOUND_PORT"
+        docker-compose run --rm -v $SRC_DIR:/dropins dsi-runtime-container1 /dsi-cmd connectivityManager deploy remote /dropins/simple_solution-0.0.esa \
+        /dropins/in-connectivity-server-configuration.xml --server=dsi-runtime-inbound --host=$DSI_IP --port=$INBOUND_PORT $SOL_MANAGER_OPTS  \
+        || echo "Connectivity deployment failed on $DSI_IP"
+
+}
+
+if [[ "$#" -ne 0 ]]; then
         print_usage
         exit 1
 fi
 
-DSI_HOME="$1"
-
 SRC_DIR=`dirname $0`
+cd SRC_DIR
+ 
+setvar ESA "$SRC_DIR/simple_solution-0.0.esa"
+setvar INCONN "$SRC_DIR/in-connectivity-server-configuration.xml"
 
-ESA="$SRC_DIR/simple_solution-0.0.esa"
-INCONN="$SRC_DIR/in-connectivity-server-configuration.xml"
+setvar CONTAINER1 `get_ip dsi-runtime-container1`
+setvar CONTAINER2 `get_ip dsi-runtime-container2`
+setvar CONTAINER3 `get_ip dsi-runtime-container3`
+setvar INBOUND `get_ip dsi-runtime-inbound`
+setvar SOL_MANAGER_OPTS "--sslProtocol=TLSv1.2 --disableServerCertificateVerification=true --disableSSLHostnameVerification=true --username=tester --password=tester"
 
-CONTAINER1=`get_ip dsi-runtime-container1`
-CONTAINER2=`get_ip dsi-runtime-container2`
-CONTAINER3=`get_ip dsi-runtime-container3`
-INBOUND=`get_ip dsi-runtime-inbound`
 
-echo "CONTAINER1=$CONTAINER1"
-echo "CONTAINER2=$CONTAINER2"
-echo "CONTAINER3=$CONTAINER3"
-echo "INBOUND=$INBOUND"
+solution_deploy $CONTAINER1 9443
+solution_deploy $CONTAINER2 9443
+solution_deploy $CONTAINER3 9443
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-	CONTAINER1_PORT=`get_host_port dsi-runtime-container1`
-	CONTAINER2_PORT=`get_host_port dsi-runtime-container2`
-	CONTAINER3_PORT=`get_host_port dsi-runtime-container3`
-	INBOUND_PORT=`get_host_port dsi-runtime-inbound`
-
-    echo "CONTAINER1_PORT=$CONTAINER1_PORT"
-    echo "CONTAINER2_PORT=$CONTAINER2_PORT"
-    echo "CONTAINER3_PORT=$CONTAINER3_PORT"
-    echo "INBOUND_PORT=$INBOUND_PORT"
-fi
-
-SOL_DEPLOY="$SRC_DIR/../solution_deploy.sh"
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    $SOL_DEPLOY $DSI_HOME localhost $CONTAINER1_PORT $ESA
-	$SOL_DEPLOY $DSI_HOME localhost $CONTAINER2_PORT $ESA
-    $SOL_DEPLOY $DSI_HOME localhost $CONTAINER3_PORT $ESA
-else
-	$SOL_DEPLOY $DSI_HOME $CONTAINER1 9443 $ESA
-	$SOL_DEPLOY $DSI_HOME $CONTAINER2 9443 $ESA
-	$SOL_DEPLOY $DSI_HOME $CONTAINER3 9443 $ESA
-fi
-
-echo "calling connectivity deploy to inbound"
-
-CONN_DEPLOY="$SRC_DIR/../conn_deploy.sh"
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-	$CONN_DEPLOY $DSI_HOME localhost $INBOUND_PORT $ESA $INCONN
-else
-	$CONN_DEPLOY $DSI_HOME $INBOUND 9443 $ESA $INCONN
-fi
+connectivity_deploy $INBOUND 9443 
