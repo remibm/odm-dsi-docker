9c9457a2c52b120d8e8114013cb891dfb32ffc87
Jean-Philippe Orsini <32877035+orsinije@users.noreply.github.com>
2018-04-03 13:47:09 +0200

    Cleanup path handling in bash fcts and scripts (#113)

--- dsi-runtime/samples/simple/run.sh
+++ dsi-runtime/samples/simple/run.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
+set -e
+
 SRC_DIR=`dirname $0`
+SRC_DIR=`realpath $SRC_DIR`
 
 function isonline() {
         SOL_MANAGER_OPTS="--host=$1 --sslProtocol=TLSv1.2 --disableServerCertificateVerification=true --disableSSLHostnameVerification=true --username=tester --password=tester"
-        pushd $SRC_DIR/../..
-        echo "Checking whether DSI is online"
         docker-compose run dsi-runtime /dsi-cmd serverManager isonline $SOL_MANAGER_OPTS
-        popd
 }
 
 function isSolutionReady() {
         SOL_MANAGER_OPTS="--host=$1 --sslProtocol=TLSv1.2 --disableServerCertificateVerification=true --disableSSLHostnameVerification=true --username=tester --password=tester"
-        pushd $SRC_DIR/../..
-        echo "Checking whether the solution is ready"
         docker-compose run dsi-runtime /dsi-cmd solutionManager isready simple_solution $SOL_MANAGER_OPTS
-        popd
 }
+
+cd $SRC_DIR
 
 echo "Starts single DSI Runtime"
 ../dsi-single-run.sh
-pushd $SRC_DIR/../..
+
+cd $SRC_DIR/../..
 CONTAINER_ID=`docker-compose ps -q`
-popd
 echo "DSI container: $CONTAINER_ID"
 
 DSI_IP=`docker exec $CONTAINER_ID hostname -I`
@@ -53,4 +52,5 @@ done
 sleep 30
 
 echo "Create a person"
-$SRC_DIR/create_person.sh $DSI_IP john.doe
+cd $SRC_DIR
+./create_person.sh $DSI_IP john.doe
