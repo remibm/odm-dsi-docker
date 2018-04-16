d49eaf5560891ac7f1776cb428b197a39926a5c0
Jean-Philippe Orsini <32877035+orsinije@users.noreply.github.com>
2018-04-16 16:51:28 +0200

    Feature/simple (#129)
    * correct json payload when an person name is passed
    
    * delayed event example and script

--- dsi-runtime/samples/simple/delayed_say_hello.sh
+++ dsi-runtime/samples/simple/delayed_say_hello.sh
@@ -4,12 +4,19 @@ set -e
 
 URL="https://localhost:9443/in/simple_json"
 
-cp ./say_hello.json /tmp/$$.json
+cp ./delayed_say_hello.json /tmp/$$.json
 
 if [ ! -z "$1" ]; then
         sed -i "s/john.doe/${1}/g" /tmp/$$.json
 fi
 
+if [ ! -z "$2" ]; then
+        echo "replace delay by $2"
+        sed -i "s/PT10S/PT${2}S/g" /tmp/$$.json
+fi
+
 curl -k -H "Content-Type: application/json" -d @/tmp/$$.json -X POST $URL
+
+cat /tmp/$$.json
 
 rm -f /tmp/$$.json
